b1dd5e200ff90df9da3a5d6c6f0d7e038994c9ba
s4ckr <s4c.kyrylo.r@gmail.com>
2025-09-13 15:21:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,37 +1,37 @@
+# ========== IMPORTS ==========
 import os 
-
 from solana.rpc.async_api import AsyncClient
-
 from aiogram import Bot, Dispatcher
 
-API_URL = 
-
-async_client = AsyncClient(API_URL)
-
 # ---------------- Telegram setup ----------------
-API_TOKEN = ""
-CHAT_ID =  
+API_TOKEN = "8220323380:AAELI7J0YuClbcilTllamshWBeyVquabvRg"             #Telegram bot token
+CHAT_ID = -4817641464                                                    #Chat id 
+
+# ---------------- Main setup ----------------
+HELIUS_API_KEY = "456ca87b-9004-4270-971a-5a9838c6f925"                  #Helius API KEY
+API_URL = f"https://mainnet.helius-rpc.com/?api-key={HELIUS_API_KEY}"    #Helius API URL
+CEXS_LIST = ["Gate"]                                                     #list of cexs you'd like to use (now "Gate" and "MEXC")
+
+MIN_SLEEP_SECONDS = 600
+MAX_SLEEP_SECONDS = 1800
+
+MIN_FUND_AMOUNT = 0.1
+MAX_FUND_AMOUNT = 0.5
+
+BALANCE_THRESHOLD =  0.00025
+
+# ========== DONT TOUCH ==========
+async_client = AsyncClient(API_URL)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-CEXS_LIST = ["Gate"]
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 PRE_JSON =  os.path.join(BASE_DIR, r"pre_balance.json")
-
 MEXC_COLD_CSV = os.path.join(BASE_DIR, r"mexc_cold.csv")
-
 GATE_COLD_CSV = os.path.join(BASE_DIR, r"gate_cold.csv")
-
 FUNDS_JSON = os.path.join(BASE_DIR, r"fundings.json")
-
 MEXC_WARM_CSV = os.path.join(BASE_DIR, r"mexc_warm.csv")
-
 GATE_WARM_CSV = os.path.join(BASE_DIR, r"gate_warm.csv")
-
 CEXS = os.path.join(BASE_DIR, r"cexs.json")
-
-
-TRANSIT_JSON = os.path.join(BASE_DIR, r"transit.json")
